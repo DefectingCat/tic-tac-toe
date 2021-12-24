331456c767491a9db0d2d51b9a3b5942ee75aa41
Defectink <i@defect.ink>
2021-12-24 21:01:40 +0800

    Add user select none

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -9,7 +9,7 @@ const Square: FC<Props> = ({ value, onClick }) => {
   return (
     <>
       <button
-        className="text-center border border-gray-300 h-10 w-10 leading-10 mt-[-1px] mr-[-1px] text-2xl"
+        className="text-center border border-gray-300 h-10 w-10 leading-10 mt-[-1px] mr-[-1px] text-2xl select-none"
         onClick={onClick}
       >
         {value}
